0996e7e354ae4a85673fd172981c2a1ce387e54b
Saad Ahmad Khan <75108541+saadak98N@users.noreply.github.com>
2020-12-09 07:44:49 +0500

    Hardware code uploaded
    This file contains the code that has been deployed on our controllers in the field.

---
+++ Arduino/Atiny_Sensor.ino
@@ -0,0 +1,257 @@
+
+#include <SoftwareSerial.h>                   //For Serial read write
+#include <OneWire.h>                          //OneWire lets you access 1-wire devices made by Maxim/Dallas
+#include <DallasTemperature.h>                //Temp Sensor
+#include <EEPROM.h>                           // For saving data in flash
+
+#define RX 4                                  
+#define TX 3
+#define buttonPin 0       // analog input pin to use as a digital input
+
+#define ONE_WIRE_BUS 1
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire); 
+SoftwareSerial SoftSerial(RX, TX);
+
+
+struct Sensor_data{
+  float soilMoisture;
+  float soilTemperature;
+};
+
+
+Sensor_data data;
+int value;
+byte buff [8];
+const byte soilMoisturePin = A1;
+float High_moisture = 0.0;
+float Low_moisture = 0.0;
+int eeAddress = 0;
+int temp = 0;
+
+//Button functions
+int checkButton();
+void clickEvent();
+void doubleClickEvent();
+void holdEvent(); 
+
+int e = 0;
+int debounce = 20;          // ms debounce period to prevent flickering when pressing or releasing the button
+int DCgap = 250;            // max ms between clicks for a double click event
+int holdTime = 1000;        // ms hold period: how long to wait for press+hold event
+int longHoldTime = 3000;    // ms long hold period: how long to wait for press+hold event
+
+// Button variables
+boolean buttonVal = LOW;   // value read from button
+boolean buttonLast = LOW;  // buffered value of the button's previous state
+boolean DCwaiting = false;  // whether we're waiting for a double click (down)
+boolean DConUp = false;     // whether to register a double click on next release, or whether to wait and click
+boolean singleOK = true;    // whether it's OK to do a single click
+long downTime = -1;         // time the button was pressed down
+long upTime = -1;           // time the button was released
+boolean ignoreUp = false;   // whether to ignore the button release because the click+hold was triggered
+boolean waitForUp = false;        // when held, whether to wait for the up event
+boolean holdEventPast = false;    // whether or not the hold event happened already
+
+
+void setup() {
+  // put your setup code here, to run once:
+SoftSerial.begin(9600);
+sensors.begin();
+pinMode(buttonPin, INPUT);
+
+
+EEPROM.get(eeAddress , High_moisture);
+eeAddress += sizeof(float);
+EEPROM.get(eeAddress , Low_moisture);
+delay(3000);
+memcpy (buff, &data, sizeof(data));
+SoftSerial.write(buff,sizeof(buff));
+
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  if (SoftSerial.available()>0){
+    value = SoftSerial.read(); 
+    delay(50);
+    if(value == 1){              //1 to read and send data
+      sensors.requestTemperatures();
+      
+      temp = analogRead(soilMoisturePin);
+      data.soilTemperature = sensors.getTempCByIndex(0);
+
+      if (temp <= High_moisture){
+        data.soilMoisture = 100;
+      }else if (temp >= Low_moisture){
+          data.soilMoisture = 0.0;
+        }else{
+        data.soilMoisture = 100*((temp - Low_moisture)/(High_moisture - Low_moisture));   //Two point Slope formula                ;
+        }
+      memcpy (buff, &data, sizeof(data));
+      SoftSerial.write(buff,sizeof(buff));
+      
+      //SoftSerial.println(data.soilMoisture);
+     // SoftSerial.println(data.soilTemperature);
+     
+     value = -1 ;
+    }
+
+
+    if (value == 9){         //9 to send High Moisture Value
+    // In setup Mode now
+    eeAddress = 0;
+    
+    // Put sensor in water
+
+    High_moisture = analogRead(soilMoisturePin);
+
+    //Send Data
+    data.soilMoisture = High_moisture ;
+
+    memcpy (buff, &data, sizeof(data));
+    SoftSerial.write(buff,sizeof(buff));
+
+    EEPROM.put(eeAddress , High_moisture);
+
+    value = -1 ;
+    }
+    
+    if(value == 0){            //0 to set low moisture value
+      eeAddress = 0;
+      eeAddress += sizeof(float);
+    //Put sensor in dry Soil
+      Low_moisture = analogRead(soilMoisturePin);
+
+  // Send data 
+    data.soilMoisture = Low_moisture ;
+
+    memcpy (buff, &data, sizeof(data));
+    SoftSerial.write(buff,sizeof(buff));
+
+    EEPROM.put(eeAddress , Low_moisture);
+    value = -1 ;
+    }
+
+  }
+  e = checkButton();
+  if (e == 1) clickEvent();
+  if (e == 2) doubleClickEvent();
+  if (e == 3) holdEvent();
+  e = 0;
+   
+  
+  delay(100);
+}
+
+void clickEvent() {
+   sensors.requestTemperatures();
+      
+      temp = analogRead(soilMoisturePin);
+      data.soilTemperature = sensors.getTempCByIndex(0);
+
+      if (temp <= High_moisture){
+        data.soilMoisture = 100;
+      }else if (temp >= Low_moisture){
+          data.soilMoisture = 0.0;
+        }else{
+        data.soilMoisture =100*((temp - Low_moisture)/(High_moisture - Low_moisture))                ;
+        }
+      memcpy (buff, &data, sizeof(data));
+      SoftSerial.write(buff,sizeof(buff));
+   
+}  
+void doubleClickEvent() {
+    eeAddress = 0;
+      eeAddress += sizeof(float);
+    //Put sensor in dry Soil
+      Low_moisture = analogRead(soilMoisturePin);
+
+  // Send data 
+    data.soilMoisture = Low_moisture ;
+
+    memcpy (buff, &data, sizeof(data));
+    SoftSerial.write(buff,sizeof(buff));
+
+    EEPROM.put(eeAddress , Low_moisture);
+    value = 0 ;
+} 
+void holdEvent() {
+   eeAddress = 0;
+    
+    // Put sensor in water
+
+    High_moisture = analogRead(soilMoisturePin);
+
+    //Send Data
+    data.soilMoisture = High_moisture ;
+
+    memcpy (buff, &data, sizeof(data));
+    SoftSerial.write(buff,sizeof(buff));
+
+    EEPROM.put(eeAddress , High_moisture);
+
+}
+
+
+int checkButton() {    
+   int event = 0;
+   buttonVal = digitalRead(buttonPin);
+   
+   // Button pressed down
+   if (buttonVal == HIGH && buttonLast == LOW  && (millis() - upTime) > debounce)
+   {
+       downTime = millis();
+       ignoreUp = false;
+       waitForUp = false;
+       singleOK = true;
+       holdEventPast = false;
+      
+       if ((millis()-upTime) < DCgap && DConUp == false && DCwaiting == true)  DConUp = true;
+       else  DConUp = false;
+       DCwaiting = false;
+   }
+   // Button released
+   else if (buttonVal == LOW && buttonLast == HIGH && (millis() - downTime) > debounce)
+   {        
+       if (not ignoreUp)
+       {
+           upTime = millis();
+           if (DConUp == false) DCwaiting = true;
+           else
+           {
+               event = 2;
+               DConUp = false;
+               DCwaiting = false;
+               singleOK = false;
+           }
+       }
+   }
+   // Test for normal click event: DCgap expired
+   if ( buttonVal == LOW && (millis()-upTime) >= DCgap && DCwaiting == true && DConUp == false && singleOK == true && event != 2)
+   {
+       event = 1;
+       DCwaiting = false;
+   }
+   // Test for hold
+   if (buttonVal == HIGH && (millis() - downTime) >= holdTime) {
+       // Trigger "normal" hold
+       if (not holdEventPast)
+       {
+           event = 3;
+           waitForUp = true;
+           ignoreUp = true;
+           DConUp = false;
+           DCwaiting = false;
+           //downTime = millis();
+           holdEventPast = true;
+       }
+        
+   }
+   buttonLast = buttonVal;
+   return event;
+}
---
+++ Arduino/Slave_Node_V2.ino
@@ -0,0 +1,141 @@
+//Slave Node Code for Agricultural Remote Sensing
+
+#include <SoftwareSerial.h> 
+#include <RHReliableDatagram.h>               //Used for LoRa module
+#include <RH_RF95.h>                          //Used for Lora module
+#include <SPI.h>
+
+#define CLIENT_ADDRESS 8
+#define SERVER_ADDRESS 2
+ 
+
+#define Reset 8
+#define RX 6
+#define TX 7
+SoftwareSerial SoftSerial(RX, TX);                 //Readind data from A3 pin of Board
+
+struct Sensor_data{
+  float soilMoisture = 0.0;
+  float soilTemperature = 0.0;
+};
+
+struct nodeData{
+  long nodeNumber = 0.0;
+  float batteryVoltage = 0.0;
+  float soilMoisture = 0.0;
+  float soilTemperature = 0.0;
+  float airTemperature = 0.0;
+  float airMoisture = 0.0;
+};
+
+
+int batteryVoltagePin = A3;  
+byte buff[sizeof(Sensor_data)];
+int Read;
+byte buf[RH_RF95_MAX_MESSAGE_LEN];
+double packetNo = 0;
+int availableBytes;
+int value = -1;
+
+Sensor_data data;
+nodeData dataFromSensor;
+
+RH_RF95 driver;
+RHReliableDatagram manager(driver, CLIENT_ADDRESS);
+
+void gatherDataFromSensors(){
+    value = 1;
+    
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(value, DEC); 
+
+    SoftSerial.write(value);
+    
+      delay(1000);
+      
+      if(SoftSerial.available()>0){
+         Serial.println("Recieving data");
+        //availableBytes = SoftSerial.available();
+        for(int n = 0 ; n < sizeof(buff); n++){
+        buff[n] = SoftSerial.read();
+        }
+        //SoftSerial.readBytes(buf,8);
+   
+        memcpy(&data , buff ,  sizeof(buff));
+        Serial.println(data.soilMoisture);
+        Serial.println(data.soilTemperature);
+        
+      }
+      else{Serial.println("Serial not Available");}
+          
+
+    dataFromSensor.soilTemperature = data.soilTemperature;
+    dataFromSensor.soilMoisture = data.soilMoisture;
+    dataFromSensor.batteryVoltage = analogRead(batteryVoltagePin)/(0.12821);   //Voltage Divider between 1k and 6.8 k
+    dataFromSensor.nodeNumber = CLIENT_ADDRESS -1 ;
+    dataFromSensor.airMoisture = 0.0;
+    dataFromSensor.airTemperature = 0.0;
+   
+   
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  SoftSerial.begin(9600);
+  Serial.begin(9600);
+  Serial.println("Starting...");
+  while (!Serial) ; 
+  if (!manager.init())
+    Serial.println("init failed");
+  pinMode(Reset,OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  delay (1000);
+    Serial.println("Analog battery voltage");
+  Serial.println(analogRead(batteryVoltagePin));
+  Serial.println("Battery Voltage");
+   Serial.println(dataFromSensor.batteryVoltage);
+
+   
+  gatherDataFromSensors();
+  
+  
+  memcpy(buf, &dataFromSensor, sizeof(dataFromSensor));
+
+  packetNo++;
+  
+  Serial.print("Sending to Server | Packet No: ");
+  Serial.println(packetNo);
+
+  
+  if (manager.sendtoWait(buf, sizeof(buf), SERVER_ADDRESS))
+  {
+ 
+    
+    // Now wait for a reply from the server
+    uint8_t len = sizeof(buf);
+    uint8_t from;   
+    if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
+    {
+      Serial.print("got reply from : 0x");
+      Serial.print(from, HEX);
+      Serial.print(": ");
+      Serial.println((char*)buf);
+    }
+    else
+    {
+      Serial.println("No reply, is the server running?");
+    }
+  }
+  else
+    Serial.println("sendtoWait failed");
+
+
+  delay(10000);
+  
+}
